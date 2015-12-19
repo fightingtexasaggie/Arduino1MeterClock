7839bb86905d69aa955ed72d2881aa12ac559687
Michael <michael@localhost.localdomain>
2015-12-18 22:14:18 -0600

    Added 5 seconds full-deflection calibration mode at startup.

--- Arduino1MeterClock.ino
+++ Arduino1MeterClock.ino
@@ -4,14 +4,24 @@
 
 RTC_DS1307 RTC;
 
-const int meterPin = 6; // PWM Pin
+const int meterPin = 5; // PWM Pin
 
 void setup() {
+  
+  // Hold max pwm modulation for 5 seconds for calibration of meter. 
+  analogWrite(meterPin, 255);
+  delay(5000);
+  // Turn off for 1 second to indicate end of calibration mode.
+  analogWrite(meterPin, 0);
+  delay(1000);
+  
   Wire.begin();
-  RTC.begin();  
+  RTC.begin();
 }
 
 void loop() {
+  
+  // get decimal time, in hours
   DateTime now = RTC.now();
   int timeHour = now.hour();
   int timeMinute = now.minute();
@@ -19,6 +29,11 @@ void loop() {
   float timeDecimal = timeHour+timeMinute/60+timeSecond/3600;
   analogWrite(meterPin, map(timeDecimal, 0, 24, 0, 255));
   delay(5000);
+  
+  // get decimal minutes, including fractional secs as fractional min
+  now = RTC.now();
+  timeMinute = now.minute();
+  timeSecond = now.second();
   float timeDecimalMin = timeMinute+timeSecond/60;
   analogWrite(meterPin, map(timeDecimalMin, 0, 60, 0, 255));
   delay(5000);
