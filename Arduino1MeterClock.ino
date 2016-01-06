020b18a7eb5947f6d26b5b4a23596dbcd41438c1
Michael <michael@localhost.localdomain>
2016-01-06 14:48:32 -0600

    Adjusted delays and fixed weird float problem messing up the fractional hours.

--- Arduino1MeterClock.ino
+++ Arduino1MeterClock.ino
@@ -5,15 +5,17 @@
 RTC_DS1307 RTC;
 
 const int meterPin = 5; // PWM Pin
+// put a 10uf capacitor across meter to smooth out pulses otherwise
+// the meter will make noise, and it's probably bad for the meter.
 
 void setup() {
   
-  // Hold max pwm modulation for 5 seconds for calibration of meter. 
+  // Hold max pwm modulation for 8 seconds for calibration of meter. 
   analogWrite(meterPin, 255);
-  delay(5000);
-  // Turn off for 1 second to indicate end of calibration mode.
+  delay(8000);
+  // Turn off for 2 second to indicate end of calibration mode.
   analogWrite(meterPin, 0);
-  delay(1000);
+  delay(2000);
   
   Wire.begin();
   RTC.begin();
@@ -26,7 +28,7 @@ void loop() {
   int timeHour = now.hour();
   int timeMinute = now.minute();
   int timeSecond = now.second();
-  float timeDecimal = timeHour+timeMinute/60+timeSecond/3600;
+  float timeDecimal = timeHour+(float)timeMinute/60.0;
   analogWrite(meterPin, map(timeDecimal, 0, 24, 0, 255));
   delay(5000);
   
@@ -34,7 +36,7 @@ void loop() {
   now = RTC.now();
   timeMinute = now.minute();
   timeSecond = now.second();
-  float timeDecimalMin = timeMinute+timeSecond/60;
+  float timeDecimalMin = timeMinute+(float)timeSecond/60.0);
   analogWrite(meterPin, map(timeDecimalMin, 0, 60, 0, 255));
   delay(5000);
 }
